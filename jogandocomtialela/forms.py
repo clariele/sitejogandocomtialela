2cd94e48662e56db61ac109a74a6a36a9068a8ee
clariele <104864858+clariele@users.noreply.github.com>
2023-05-04 22:06:10 -0300

    Add files via upload

---
+++ jogandocomtialela/forms.py
@@ -0,0 +1,48 @@
+from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from jogandocomtialela.models import Usuario
+from flask_login import current_user
+
+
+class FormCriarConta(FlaskForm):
+    username = StringField('Nome de Usuário', validators=[DataRequired()])
+    email = StringField('E-mail', validators=[DataRequired(), Email()])
+    senha = PasswordField('Senha', validators=[DataRequired(), Length(6,20)])
+    confirmacao = PasswordField('Confirmação da Senha', validators=[DataRequired(), EqualTo('senha')])
+    botao_submit_criarconta = SubmitField('Criar Conta')
+
+    def validate_email(self, email):
+        usuario = Usuario.query.filter_by(email=email.data).first()
+        if usuario:
+            raise ValidationError('E-mail já cadastrado. Cadastre-se com outro e-mail ou faça login para continuar')
+
+
+class FormLogin(FlaskForm):
+
+    email = StringField('E-mail', validators=[DataRequired(), Email()])
+    senha = PasswordField('Senha', validators=[DataRequired(), Length(6,20)])
+    lembrar_dados = BooleanField('Lembrar Dados de Acesso')
+    botao_submit_login = SubmitField('Fazer Login')
+
+class FormEditarPerfil(FlaskForm):
+    username = StringField('Nome de Usuário', validators=[DataRequired()])
+    email = StringField('E-mail', validators=[DataRequired(), Email()])
+    foto_perfil = FileField('Atualizar Foto de Perfil', validators=[FileAllowed(['jpg', 'png', 'jpeg'])])
+    jogo_cacapalavras = BooleanField('Caça Palavras')
+    jogo_pacman = BooleanField('Pac Man')
+    jogo_rima = BooleanField('Jogo da Rima')
+    jogo_flappybird = BooleanField('Flappy Bird')
+    botao_submit_editarperfil = SubmitField('Confirmar Edição')
+
+    def validate_email(self, email):
+        if current_user.email != email.data:
+            usuario = Usuario.query.filter_by(email=email.data).first()
+            if usuario:
+                raise ValidationError('Já existe um usuário com este e-mail. Cadastre outro e-mail')
+
+class FormCriarPost(FlaskForm):
+    titulo = StringField('Título do Post', validators=[DataRequired(), Length(2, 140)])
+    corpo = TextAreaField('Escreva seu Post aqui', validators=[DataRequired()])
+    botao_submit = SubmitField('Criar Post')
---
+++ jogandocomtialela/teste.py
@@ -0,0 +1,16 @@
+from jogandocomtialela import app, database
+from jogandocomtialela.models import Usuario
+
+#with app.app_context():
+#    database.create_all()
+
+# with app.app_context():
+#     Usuario.query.all()
+#     meus_usuarios = Usuario.query.filter_by(username='victor').first()
+#     print(meus_usuarios.username)
+#     print(meus_usuarios.email)
+#     print(meus_usuarios.senha)
+
+# with app.app_context():
+#     database.drop_all()
+#     database.create_all()
